9ee98606b3981815f7b15afcaa3112605f443605
Alexander Banh <alexbanh1@Alexanders-MacBook-Pro.local>
2017-03-18 15:28:19 -0400

    assignment 11 in progress

--- scripts/album.js
+++ scripts/album.js
@@ -15,18 +15,33 @@ var albumPicasso = {
      ]
  };
  
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumMonkeys = {
+     title: 'King Kong',
+     artist: 'Arctic Monkeys',
+     label: 'UMG',
+     year: '2020',
+     albumArtUrl: 'assets/images/album_covers/14.png',
      songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
+         { title: 'Bananas', duration: '1:01' },
+         { title: 'Banana Bread', duration: '5:01' },
+         { title: 'Banana Cake', duration: '3:21'},
+         { title: 'Banana Smoothie', duration: '3:14' },
+         { title: 'No More Bananas', duration: '2:15'}
      ]
  };
 
@@ -42,14 +57,15 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+// #1 selects all the relevant elements & assigns to vars
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-     // #1 selects all the relevant elements & assigns to vars
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
+    
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -66,5 +82,15 @@ var setCurrentAlbum = function(album) {
  };
  
  window.onload = function() {
-     setCurrentAlbum(albumPicasso);
+    setCurrentAlbum(albumPicasso);
+    
+    var albumList = [albumPicasso, albumMarconi, albumMonkeys];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(album[index]);  
+        index++;
+        if (index == albumList.length) {
+            index = 0;
+        };
+    });
  };
